b40bc8489cf40f0b5c42597602506093299298b7
Rodolfo Araya <rodolfo.araya@multiwireless.cl>
2018-07-19 12:37:46 -0400

    Agrega destroy, edit y update en 'Articles'

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,20 +14,41 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   #POST /articles
   def create
-    @article = Article.new(title: params[:article][:title],
-                           body: params[:article][:body])
+    @article = Article.new(article_params)
     if @article.save
       redirect_to @article
     else
       render :new
     end
+  end
 
+  #DELETE /articles/:id
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
   end
 
   #PUT /articles/:id
   def update
-
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit
+    end
   end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
+
 end
